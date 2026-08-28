f6395bf58d74fced090fb08ddbf006c9a4b92bc5
AmirZalaghi <zalaghi.dev@gmail.com>
2026-08-28 18:32:27 +0330

    fix: port review

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 4003,
+  },
+  preview: {
+    port: 4003,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
