21aac09ea7cc39a59033d7bb0f9d06305b1684ef
AmirZalaghi <zalaghi.dev@gmail.com>
2026-08-28 16:24:35 +0330

    feat: font & components

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { DirectionProvider } from "@/components/ui/direction";
+import "@/styles/fonts-iran-yekan.css";
 import App from "./App.tsx";
 import "./index.css";
 
